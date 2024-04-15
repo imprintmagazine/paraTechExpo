f44a4d7cd1f5cbaf79630b902c01834b03f51057
Imprint <imprintmagazine84@gmail.com>
2024-04-15 14:34:16 +0530

    changes

---
+++ assets/js/visitorBenefits.js
@@ -0,0 +1,35 @@
+// visit benefits start here====================================================
+var newWhyVisitor = document.querySelector(".visitBenefitsInfo");
+
+newWhyVisitor.innerHTML = `
+<div>
+<p><b>Information:</b> Latest innovation to asist your solution.</p>
+</div>
+
+<div>
+<p><b>Strategy:</b> Discover strategy to integrate automation in your manufacturing. PARA Tech Expo first mantra &gt;&gt; Industrial automation leads to excellenc &lt;&lt;</p>
+</div>
+
+<div>
+<p><b>Talent:</b> Meet professionals with expertise join your company. Imprint alert 1 &gt;&gt; Grave storage of engineers and lack of talent &lt;&lt;</p>
+</div>
+
+<div>
+<p><b>Future security:</b> Adopt future proof automation solutions secure ROI on investment in automation.</p>
+</div>
+
+<div>
+<p><b>Analys:</b> Understand your product automated manufacturing vis-avis copatative solution and process select the most appropriate automation.</p>
+</div>
+
+<div>
+<p><b>Knowledge:</b> Benefit from your presence to attend meaningful conference sessions and seminars.</p>
+</div>
+
+<div>
+<p><b>Opportunity:</b> Professionals and companies attend Industries specific association concourse for your product and automation solutions or service.</p>
+</div>
+`;
+// // visit benefits end here====================================================
+
+
--- assets/js/visitorProfile.js
+++ assets/js/visitorProfile.js
@@ -1,38 +1,5 @@
-// why visit start here====================================================
-var newWhyVisitor = document.querySelector(".visitBenefitsInfo");
 
-newWhyVisitor.innerHTML = `
-<div>
-<p><b>Information:</b> Latest innovation to asist your solution.</p>
-</div>
-
-<div>
-<p><b>Strategy:</b> Discover strategy to integrate automation in your manufacturing. PARA Tech Expo first mantra &gt;&gt; Industrial automation leads to excellenc &lt;&lt;</p>
-</div>
-
-<div>
-<p><b>Talent:</b> Meet professionals with expertise join your company. Imprint alert 1 &gt;&gt; Grave storage of engineers and lack of talent &lt;&lt;</p>
-</div>
-
-<div>
-<p><b>Future security:</b> Adopt future proof automation solutions secure ROI on investment in automation.</p>
-</div>
-
-<div>
-<p><b>Analys:</b> Understand your product automated manufacturing vis-avis copatative solution and process select the most appropriate automation.</p>
-</div>
-
-<div>
-<p><b>Knowledge:</b> Benefit from your presence to attend meaningful conference sessions and seminars.</p>
-</div>
-
-<div>
-<p><b>Opportunity:</b> Professionals and companies attend Industries specific association concourse for your product and automation solutions or service.</p>
-</div>
-`;
-// why visit end here====================================================
-
-// why should visitor start here========================================================
+// profile visitor start here========================================================
 
 // Create a new div element for the new visitor
 var newVisitorDiv = document.querySelector(".visitorProfileInfo");
@@ -91,6 +58,4 @@ newVisitorDiv.innerHTML = `
           </div>
         </div>
 `;
-// why should visitor start here========================================================
-
-
+// profile visitor start here========================================================
